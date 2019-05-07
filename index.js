fbab01bea9704ae7e465b76b9633f57f81811abc
Magno Valdetaro <mvaldetaro@gmail.com>
2019-05-07 16:07:49 -0300

    Add isOnline

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-exports.renders = require("./lib/utils/renders");
-exports.localStorages = require("./lib/utils/localStorages");
+exports.renders = require('./lib/utils/renders');
+exports.localStorages = require('./lib/utils/localStorages');
+exports.validators = require('./lib/utils/validators');
 
-exports.bindStateToLocalStorage = require("./lib/middleware/bindStateToLocalStorage");
+exports.bindStateToLocalStorage = require('./lib/middleware/bindStateToLocalStorage');
