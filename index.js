5f424fe2cfe6ac6d07ac2d2c2c1a9151d9094548
Magno Valdetaro <mvaldetaro@gmail.com>
2019-05-17 16:21:46 -0300

    browser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.renders = require('./lib/utils/renders');
 exports.localStorages = require('./lib/utils/localStorages');
 exports.validators = require('./lib/utils/validators');
+exports.browsers = require('./lib/utils/browsers');
 
 exports.bindStateToLocalStorage = require('./lib/middleware/bindStateToLocalStorage');
