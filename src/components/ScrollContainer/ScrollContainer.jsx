af6f32cb0881617aa8820f23eea56bc3352e98e8
Magno Valdetaro <mvaldetaro@gmail.com>
2019-06-13 18:09:19 -0300

    Add ScrollContainer

---
+++ src/components/ScrollContainer/ScrollContainer.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Style from './ScrollContainer.module.scss';
+//import { momentumScroll } from './lib/behaviors';
+import { momentumScroll } from '../../../lib/behaviors';
+
+class ScrollContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.containerRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.momentumScroll = momentumScroll(this.containerRef.current, {
+            speed: this.props.speed,
+            duration: this.props.duration,
+            directions: this.props.directions
+        }).mount();
+    }
+
+    componentWillUnmount() {
+        this.momentumScroll.unmount();
+    }
+
+    render() {
+        const xClass = classNames(Style.root, this.props.className, {});
+        return (
+            <div
+                className={xClass}
+                ref={this.containerRef}
+                style={this.props.style}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+ScrollContainer.propTypes = {
+    speed: PropTypes.number,
+    duration: PropTypes.number,
+    directions: PropTypes.number
+};
+
+ScrollContainer.defaultProps = {
+    speed: 2,
+    duration: 2,
+    directions: 2
+};
+
+export default ScrollContainer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as View } from './View';
 export { default as Deck } from './Deck';
+export { default as ScrollContainer } from './ScrollContainer';
