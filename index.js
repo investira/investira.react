1e841dc1d2cb802ba1bcea63d56c43b2dcac42b3
Magno Valdetaro <mvaldetaro@gmail.com>
2019-06-17 12:24:15 -0300

    Add bindStateToLocalStorage

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ exports.renders = require('./lib/utils/renders');
 exports.localStorages = require('./lib/utils/localStorages');
 exports.validators = require('./lib/utils/validators');
 exports.browsers = require('./lib/utils/browsers');
-
 exports.bindStateToLocalStorage = require('./lib/middleware/bindStateToLocalStorage');
