3851f5d568cca0174b9173117493d74f5f05daff
Magno Valdetaro <mvaldetaro@gmail.com>
2019-06-17 12:23:16 -0300

    Arquivos de estilo comum a todos os componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
+import './Common/_core.scss';
+import './Common/reset.scss';
+import './Common/base.scss';
+import './Common/utils.scss';
+
+import { themePrimary } from './Common/invThemes';
+
 export { default as AppBar } from './AppBar';
 export { default as View } from './View';
 export { default as Deck } from './Deck';
