878009c2bda85ee4ac021354b69a8d130aa36484
Ricardo Villar <ricardo.villar@investira.com.br>
2019-06-29 13:32:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ require('./lib/polyfills');
 exports.charts = require('./lib/utils/charts');
 exports.renders = require('./lib/utils/renders');
 exports.localStorages = require('./lib/utils/localStorages');
-exports.validators = require('./lib/utils/validators');
 exports.browsers = require('./lib/utils/browsers');
 exports.bindStateToLocalStorage = require('./lib/middleware/bindStateToLocalStorage');
