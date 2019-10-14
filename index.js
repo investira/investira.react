74d124485892ee4882da1bf163131122af69bb23
Magno Valdetaro <mvaldetaro@gmail.com>
2019-10-14 16:08:16 -0300

    Add formatação por mascara de agência e conta

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('./lib/polyfills');
 
 exports.charts = require('./lib/utils/charts');
+exports.displays = require('./lib/utils/displays');
 exports.renders = require('./lib/utils/renders');
 exports.localStorages = require('./lib/utils/localStorages');
 exports.browsers = require('./lib/utils/browsers');
+exports.currency = require('./lib/utils/currency');
 exports.bindStateToLocalStorage = require('./lib/middleware/bindStateToLocalStorage');
