c55c96ec93854d5a885f6b11f2aaa9c86cd2358c
Magno Valdetaro <mvaldetaro@gmail.com>
2020-01-22 12:48:26 -0300

    Define uma página inicial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import * as components from "./components";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './views/Root';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Root.module.css';
+
+function Root() {
+    return (
+        <div className={style.root}>
+            <h1>React Lib</h1>
+        </div>
+    );
+}
+
+export default Root;
