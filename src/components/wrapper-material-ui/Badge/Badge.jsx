1e82c6dd69096a46c91ade5e115c4a687c6d54c0
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2020-02-28 17:19:54 -0300

    Revert "upload de fotos no perfil retornando o path da imagem"
    This reverts commit 51d21b30f715ae0f9a3b7c1481be95fb52911818.

--- src/components/wrapper-material-ui/Badge/Badge.jsx
+++ src/components/wrapper-material-ui/Badge/Badge.jsx
@@ -1,41 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+
 import { default as WBadge } from '@material-ui/core/Badge';
-import { classList } from '../../utils/helpers';
 
-const useStyles = makeStyles(
-    theme => ({
-        large: {
-            '& $badge': {
-                width: theme.spacing(4),
-                height: theme.spacing(4),
-                borderRadius: theme.spacing(3),
-                '& svg': {
-                    width: '21px',
-                    height: '21px'
-                }
-            }
-        },
-        badge: {}
-    }),
-    { name: 'MuiBadge' }
-);
-
-function Badge(props) {
-    const { anchorOrigin, ...propsProps } = props;
-    const classes = useStyles();
-
-    const xClassNames = {
-        [classes.large]: props.size === 'large'
-    };
-
-    return (
-        <WBadge
-            {...propsProps}
-            anchorOrigin={anchorOrigin}
-            className={classList(xClassNames)}
-        />
-    );
+class Badge extends Component {
+    render() {
+        return <WBadge {...this.props} />;
+    }
 }
 
 export default Badge;
--- src/components/wrapper-material-ui/KeyboardDatePicker/KeyboardDatePicker.jsx
+++ src/components/wrapper-material-ui/KeyboardDatePicker/KeyboardDatePicker.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import MomentUtils from '@date-io/moment';
+import React from "react";
+import MomentUtils from "@date-io/moment";
 //import { Icon } from '../../app/components';
 import {
-    KeyboardDatePicker as WKeyboardDatePicker,
-    MuiPickersUtilsProvider
-} from '@material-ui/pickers/';
+  KeyboardDatePicker as WKeyboardDatePicker,
+  MuiPickersUtilsProvider
+} from "@material-ui/pickers/";
 
-import 'moment/min/locales';
+import "moment/min/locales";
 
 const KeyboardDatePicker = props => {
-    return (
-        <MuiPickersUtilsProvider utils={MomentUtils} locale={props.locale}>
-            <WKeyboardDatePicker
-                {...props}
-                // keyboardIcon={<Icon iconName="calendar" size="21" />}
-                refuse={/[^\d]+/gi}
-                cancelLabel="fechar"
-            />
-        </MuiPickersUtilsProvider>
-    );
+  console.log(props);
+  return (
+    <MuiPickersUtilsProvider utils={MomentUtils} locale={props.locale}>
+      <WKeyboardDatePicker
+        {...props}
+        // keyboardIcon={<Icon iconName="calendar" size="21" />}
+        refuse={/[^\d]+/gi}
+        cancelLabel="fechar"
+      />
+    </MuiPickersUtilsProvider>
+  );
 };
 
 export default KeyboardDatePicker;
