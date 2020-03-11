92e84fde92ddb6941dd3d31f21a404f312a27f7f
Magno Valdetaro <mvaldetaro@gmail.com>
2020-03-11 10:47:06 -0300

    Add SearchBox e FluidPaper

---
+++ src/components/FilterBar/FilterBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Icon } from 'investiraComponents';
+import PropTypes from 'prop-types';
+import Style from './FilterBar.module.scss';
+
+const FilterBar = props => {
+    const [active, setActive] = useState(props.initialValue);
+
+    const handleClick = pEvent => {
+        const xDataset = pEvent.currentTarget.dataset;
+        setActive(Number(xDataset.index));
+        props.onClick && props.onClick(xDataset.index, Number(xDataset.value));
+    };
+
+    const xData = props.data || [];
+
+    return (
+        <div className={Style.root}>
+            <nav className={Style.nav}>
+                {xData.map((xItem, xIndex) => {
+                    return (
+                        <button
+                            id={`btn-filter-${xItem.value}`}
+                            data-value={xItem.value}
+                            data-index={xIndex}
+                            key={xIndex}
+                            className={`${Style.button} ${active === xIndex ? Style.active : null}`}
+                            onClick={handleClick}>
+                            <Icon
+                                className={Style.icon}
+                                iconName={xItem.iconName}
+                                color={active === xIndex ? xItem.default : xItem.color}
+                            />
+                            {xItem.label}
+                        </button>
+                    );
+                })}
+            </nav>
+        </div>
+    );
+};
+
+FilterBar.propTypes = {
+    onClick: PropTypes.func,
+    data: PropTypes.array
+};
+
+export default FilterBar;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,182 @@
+import React, { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { InputBase, IconButton, Divider, FormControl, Chip, Icon } from 'investiraComponents';
+
+import { validators } from 'investira.sdk';
+import Style from './SearchBox.module.scss';
+
+const SearchBox = props => {
+    const [clearBtn, setClearBtn] = useState(false);
+    const [value, setValue] = useState(props.value || '');
+    const [querySplited, setQuerySplited] = useState(props.value ? props.value.split(' ') : []);
+
+    const searchRef = useRef();
+
+    const handleClear = pEvent => {
+        setValue('');
+        setClearBtn(false);
+        setQuerySplited([]);
+        props.clearCallback && props.clearCallback(pEvent);
+    };
+
+    const handleChange = pEvent => {
+        const xValue = pEvent.currentTarget.value;
+        //setValue(xValue);
+        updateValue(xValue);
+    };
+
+    const querySplit = xQuery => {
+        //verifica se tem vírgula ou ponto e vírgula
+        const xRegex = /,|;/gi;
+        const xMatch = validators.isNull(xQuery.match(xRegex));
+        const xQuerySplited = xQuery.split(xMatch ? ' ' : xRegex);
+        return xQuerySplited;
+    };
+
+    const updateValue = pValue => {
+        setValue(pValue);
+        //querySplit(pValue);
+
+        if (pValue.length >= 1) {
+            setClearBtn(true);
+            setQuerySplited(querySplit(pValue));
+        } else {
+            setClearBtn(false);
+            setQuerySplited([]);
+        }
+
+        props.onChange && props.onChange(pValue);
+    };
+
+    const handleFilter = () => {
+        props.onClickFilter && props.onClickFilter(true);
+    };
+
+    const handleDelete = chipToDelete => {
+        const filtered = querySplited.filter(xValue => {
+            return xValue !== chipToDelete;
+        });
+
+        setQuerySplited(filtered);
+        updateValue(filtered.join(' '));
+    };
+
+    const closeKeyboard = {
+        mount: () => {
+            document.addEventListener('keydown', pEvent => {
+                if (searchRef.current && pEvent.keyCode === 13) {
+                    searchRef.current.blur();
+                }
+            });
+        },
+        unmount: () => {
+            document.removeEventListener('keydown', pEvent => {
+                if (searchRef.current && pEvent.keyCode === 13) {
+                    searchRef.current.blur();
+                }
+            });
+        }
+    };
+
+    useEffect(() => {
+        props.fowardRef && props.fowardRef(searchRef);
+        closeKeyboard.mount();
+
+        return () => {
+            closeKeyboard.unmount();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (props.value && props.value.length > 0) {
+            updateValue(props.value || '');
+        }
+    }, [props.value]);
+
+    return (
+        <div className={Style.root}>
+            <div className={Style.searchWrap}>
+                <div className={Style.inputWrap}>
+                    <FormControl fullWidth>
+                        <InputBase
+                            inputRef={searchRef}
+                            type="search"
+                            id={props.id}
+                            value={value}
+                            onChange={handleChange}
+                            onFocus={props.onFocus}
+                            onBlur={props.onBlur}
+                            placeholder={props.placeholder}
+                            inputProps={props.inputProps}
+                            startAdornment={
+                                !props.onClick ? (
+                                    <Icon
+                                        iconName={'find'}
+                                        className={Style.adornement}
+                                        color={'secondaryLight'}
+                                    />
+                                ) : null
+                            }
+                        />
+                    </FormControl>
+                </div>
+                {clearBtn && (
+                    <IconButton aria-label="busca" onClick={handleClear}>
+                        <Icon iconName={'cancel'} size={'16'} />
+                    </IconButton>
+                )}
+
+                {props.onClick && (
+                    <IconButton aria-label="busca" onClick={props.onClick}>
+                        <Icon iconName={'find'} />
+                    </IconButton>
+                )}
+
+                {props.filter && (
+                    <IconButton color="primary" aria-label="filtro" onClick={handleFilter}>
+                        <Icon iconName={'filter'} />
+                    </IconButton>
+                )}
+            </div>
+            <Divider />
+            {!validators.isEmpty(querySplited) && (
+                <div className={Style.querys}>
+                    {querySplited.map((xData, xIndex) => {
+                        if (validators.isEmpty(xData)) {
+                            return null;
+                        } else {
+                            return (
+                                <div className={Style.chip} key={xIndex}>
+                                    <Chip
+                                        variant={'outlined'}
+                                        color={'primary'}
+                                        label={xData}
+                                        size={'small'}
+                                        onDelete={() => handleDelete(xData)}
+                                    />
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+            )}
+        </div>
+    );
+};
+
+SearchBox.propTypes = {
+    id: PropTypes.string,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    inputProps: PropTypes.object,
+    filter: PropTypes.bool
+};
+
+SearchBox.defaultProps = {
+    filter: false,
+    id: 'searchbox'
+};
+
+export default React.memo(SearchBox);
