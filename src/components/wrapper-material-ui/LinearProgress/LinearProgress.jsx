99c38882b61ecb2b8c3702b59d5df2c4bfa5b62d
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2020-04-02 12:54:41 -0300

    add LinearProgress

---
+++ src/components/wrapper-material-ui/LinearProgress/LinearProgress.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { default as WLinearProgress } from '@material-ui/core/LinearProgress';
+
+function LinearProgress(props) {
+    return <WLinearProgress {...props} />;
+}
+
+export default LinearProgress;
