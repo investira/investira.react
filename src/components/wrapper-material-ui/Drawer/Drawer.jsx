a641cf91a5e42d403b4d6f9b768808fa483eb572
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2020-07-09 17:02:44 -0300

    Update Drawer.jsx

--- src/components/wrapper-material-ui/Drawer/Drawer.jsx
+++ src/components/wrapper-material-ui/Drawer/Drawer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { default as WDrawer } from '@material-ui/core/Drawer';
 import { classList } from '../../utils/helpers';
