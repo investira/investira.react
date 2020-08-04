27f773c178b064073d85156267baa7e24e188ce6
Magno Valdetaro <mvaldetaro@gmail.com>
2020-08-04 17:34:13 -0300

    Remove código não usado

--- src/components/CrudProvider/CrudProvider.jsx
+++ src/components/CrudProvider/CrudProvider.jsx
@@ -2,20 +2,11 @@ import React, { memo, useState } from 'react';
 import CrudContext from '../CrudContext';
 
 const CrudProvider = memo(props => {
-    const { data, pages } = props.values;
-
-    // const xState = {
-    //     listSize: data ? Object.values(data).length : 0
-    // };
-
-    //console.log(xState);
-
     const onCreate = pValues => {
         props.values.onCreate && props.values.onCreate(pValues);
     };
 
     const onRead = pParams => {
-        //console.log(pParams);
         props.values.onRead && props.values.onRead(pParams);
     };
 
