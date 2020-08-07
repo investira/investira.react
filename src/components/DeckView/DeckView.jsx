20bcc3287ae21f1da4d5a93ff635529dc482869e
Magno Valdetaro <mvaldetaro@gmail.com>
2020-08-07 15:46:33 -0300

    Exibe o nome para debug

--- src/components/DeckView/DeckView.jsx
+++ src/components/DeckView/DeckView.jsx
@@ -1,10 +1,18 @@
 import React, { memo, useContext } from 'react';
 import { DeckContext } from '../';
 
-const DeckView = memo(props => {
-    const deckContext = useContext(DeckContext);
+const DeckView = memo(
+    Object.assign(
+        props => {
+            const deckContext = useContext(DeckContext);
 
-    return React.cloneElement(props.children, { id: props.id, ...deckContext });
-});
+            return React.cloneElement(props.children, {
+                id: props.id,
+                ...deckContext
+            });
+        },
+        { displayName: 'DeckView' }
+    )
+);
 
 export default DeckView;
