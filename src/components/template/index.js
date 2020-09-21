42462b87f845c125619e2307b8d66d4ba6381aee
Magno Valdetaro <mvaldetaro@gmail.com>
2020-09-21 09:51:55 -0300

    Add Skeleton

--- src/components/template/index.js
+++ src/components/template/index.js
@@ -3,3 +3,4 @@ export { default as Hiddenable } from './Hiddenable';
 export { default as Loading } from './Loading';
 export { default as Placeholder } from './Placeholder';
 export { default as ResponsiveImage } from './ResponsiveImage';
+export { default as Skeleton } from './Skeleton';
