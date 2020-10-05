42b6e21723132c39da607670765146cb8445764f
Magno Valdetaro <mvaldetaro@gmail.com>
2020-10-05 14:29:25 -0300

    Ajusta a animação e o svg

--- src/components/template/Success/Success.jsx
+++ src/components/template/Success/Success.jsx
@@ -16,28 +16,14 @@ function Success(props) {
                     y="0px"
                     width={props.width ? `${props.width}px` : null}
                     height={props.height ? `${props.height}px` : null}
-                    viewBox="0 0 200 200"
+                    viewBox="0 0 512 512"
                     className={props.startAnimation ? Style.start : undefined}>
                     <path
-                        fill="none"
-                        stroke="#00DFA8"
-                        strokeWidth="6"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeMiterlimit="10"
-                        d="M7.688000000000002,99.625A93,93 0,1,1 193.688,99.625A93,93 0,1,1 7.688000000000002,99.625"
-                        className={Style.ok}
-                    />
-                    <path
-                        fill="none"
-                        stroke="#47E0A8"
-                        strokeWidth="7"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeMiterlimit="10"
-                        d="M47.562,98.375L83.562,134.625L153.812,64.625"
                         className={Style.circle}
-                    />
+                        d="M22.49999999999997,255.2A234.4,234.4 0,1,1 491.29999999999995,255.2A234.4,234.4 0,1,1 22.49999999999997,255.2"></path>
+                    <path
+                        className={Style.ok}
+                        d="M154.7,256.7L224.6,328.7L357.3,183.3"></path>
                 </svg>
             </div>
         </div>
