1cbc52593d819dede15b54275ce75870b6e05398
Magno Valdetaro <mvaldetaro@gmail.com>
2020-10-15 08:21:40 -0300

    Corrige a exibição do loading quando altera a pesquisa

--- src/components/ListState/ListState.jsx
+++ src/components/ListState/ListState.jsx
@@ -5,7 +5,17 @@ import PropTypes from 'prop-types';
 import Style from './ListState.module.scss';
 
 const ListMessage = memo(props => {
-    if (props.isFetching && props.listSize <= 0) {
+    // if (props.isFetching && props.listSize <= 0) {
+    //     return (
+    //         <div className={Style.root}>
+    //             <CenterInView>
+    //                 <Loading />
+    //             </CenterInView>
+    //         </div>
+    //     );
+    // }
+
+    if (props.isFetching) {
         return (
             <div className={Style.root}>
                 <CenterInView>
