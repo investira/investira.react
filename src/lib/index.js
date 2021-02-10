84915e11a39aee48393dc53ecf8fe7cdf9df53e8
Magno Valdetaro <mvaldetaro@gmail.com>
2021-02-10 11:32:16 -0300

    Implementa enigine do IndexedDb para redux-persist

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,6 +6,7 @@ export { default as charts } from './utils/charts';
 export { default as browsers } from './utils/browsers';
 export { default as localStorages } from './utils/localStorages';
 export { default as lists } from './utils/lists';
+export { default as vibrate } from './utils/vibrate';
 
 // Middleware
 export { default as bindStateToLocalStorage } from './middleware/bindStateToLocalStorage';
@@ -21,6 +22,7 @@ export { default as persisterReducers } from './redux/persisterReducers';
 export { default as createStorage } from './storage/createStorage';
 export { default as listenStorage } from './storage/listenStorage';
 export { default as pouchStorage } from './storage/pouchStorage';
+export { default as idbStorage } from './storage/idbStorage';
 
 // HOCs
 export { default as withDialog } from './hoc/withDialog';
