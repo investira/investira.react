fedcbd852109824143aef017d2472a477756fa42
Magno Valdetaro <mvaldetaro@gmail.com>
2021-03-31 12:23:23 -0300

    Update Tabs.jsx

--- src/components/wrapper-material-ui/Tabs/Tabs.jsx
+++ src/components/wrapper-material-ui/Tabs/Tabs.jsx
@@ -2,20 +2,8 @@ import React, { memo } from 'react';
 import { default as WTabs } from '@material-ui/core/Tabs';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
-    tabsRoot: {
-        padding: '0 16px'
-    }
-});
-
 const Tabs = memo(props => {
-    const { classes } = props;
-
-    return (
-        <div className={classes.root}>
-            <WTabs {...props} classes={{ root: classes.tabsRoot }} />
-        </div>
-    );
+    return <WTabs {...props} />;
 });
 
-export default withStyles(styles)(Tabs);
+export default Tabs;
