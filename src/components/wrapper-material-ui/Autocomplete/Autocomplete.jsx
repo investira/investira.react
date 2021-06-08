22d9ada3718d8775aa965c67cf36c22cf38c691e
Magno Valdetaro <mvaldetaro@gmail.com>
2021-06-08 18:30:05 -0300

    Update Autocomplete.jsx

--- src/components/wrapper-material-ui/Autocomplete/Autocomplete.jsx
+++ src/components/wrapper-material-ui/Autocomplete/Autocomplete.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { default as MuiAutocomplete } from '@material-ui/lab/Autocomplete';
+import {
+    default as MuiAutocomplete,
+    createFilterOptions
+} from '@material-ui/lab/Autocomplete';
 
 function Autocomplete(props) {
-    return <MuiAutocomplete {...props} />;
+    const filter = createFilterOptions();
+
+    const filterOptions = (pOptions, pParams) => {
+        const filtered = filter(pOptions, pParams);
+
+        // Sugere a criacão de um novo valor
+        if (pParams.inputValue !== '') {
+            filtered.push({
+                inputValue: pParams.inputValue,
+                label: `Add "${pParams.inputValue}"`
+            });
+        }
+
+        return filtered;
+    };
+
+    return <MuiAutocomplete {...props} filterOptions={filterOptions} />;
 }
 
 export default Autocomplete;
