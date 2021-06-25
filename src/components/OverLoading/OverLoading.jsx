9c4a2054326883e278c7586c0850b272c8433e3e
Magno Valdetaro <mvaldetaro@gmail.com>
2021-06-25 13:02:50 -0300

    Suporte a mensagens

--- src/components/OverLoading/OverLoading.jsx
+++ src/components/OverLoading/OverLoading.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
-import { CircularProgress, Backdrop } from '../wrapper-material-ui';
+import { CircularProgress, Backdrop, Typography } from '../wrapper-material-ui';
 
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     backdrop: {
         zIndex: theme.zIndex.drawer + 1
+    },
+    info: {
+        margin: '0 auto',
+        width: '60%',
+        textAlign: 'center'
+    },
+    hspace: {
+        height: '12px'
     }
 }));
 
@@ -27,7 +35,18 @@ function OverLoading(props) {
             open={open}
             //onClick={handleClose}
         >
-            <CircularProgress color="primary" />
+            <div className={classes.info}>
+                <CircularProgress color="primary" />
+                <div className={classes.hspace}></div>
+                {props.message && (
+                    <Typography
+                        color="textPrimary"
+                        align="center"
+                        variant="body2">
+                        {props.message}
+                    </Typography>
+                )}
+            </div>
         </Backdrop>
     );
 }
