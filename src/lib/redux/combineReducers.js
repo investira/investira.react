65f9b510430016aedf53188ed9ecb8331d802aa7
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2021-07-19 10:09:15 -0300

    não salva connected do websocket no local

--- src/lib/redux/combineReducers.js
+++ src/lib/redux/combineReducers.js
@@ -1,11 +1,21 @@
 import { combineReducers as mergeReducers } from 'redux';
 import persisterReducers from './persisterReducers';
 
-const combineReducers = (pReducers = {}, pStoreName, pStorage) => {
+const combineReducers = (
+    pReducers = {},
+    pStoreName,
+    pStorage,
+    pBlacklist = []
+) => {
     let xReducers = pReducers;
 
     if (pStoreName) {
-        xReducers = persisterReducers(xReducers, pStoreName, pStorage);
+        xReducers = persisterReducers(
+            xReducers,
+            pStoreName,
+            pStorage,
+            pBlacklist
+        );
     }
 
     return mergeReducers(xReducers);
--- src/lib/redux/persisterReducers.js
+++ src/lib/redux/persisterReducers.js
@@ -1,25 +1,21 @@
 import { persistReducer } from 'redux-persist';
 import createStorage from '../storage/createStorage';
 
-const persiterReducers = (pReducers, pPrefix, pStorage) => {
+const persisterReducers = (pReducers, pPrefix, pStorage, pBlacklist = []) => {
     const xEntries = Object.entries(pReducers);
     const xStorage = createStorage(pStorage);
 
     const xReducers = xEntries.map(xEntry => {
-        return [
-            xEntry[0],
-            persistReducer(
-                {
-                    key: `${pPrefix}-${xEntry[0]}`,
-                    storage: xStorage,
-                    timeout: 0
-                },
-                xEntry[1]
-            )
-        ];
+        let xAttributes = {
+            key: `${pPrefix}-${xEntry[0]}`,
+            storage: xStorage,
+            timeout: 0
+        };
+        xAttributes.blacklist = pBlacklist;
+        return [xEntry[0], persistReducer(xAttributes, xEntry[1])];
     });
 
     return Object.fromEntries(xReducers);
 };
 
-export default persiterReducers;
+export default persisterReducers;
