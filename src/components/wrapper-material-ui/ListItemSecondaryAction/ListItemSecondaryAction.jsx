32d208ec5a8d848783022df13ba4e093ae52b4cf
Magno Valdetaro <mvaldetaro@gmail.com>
2021-07-23 10:33:03 -0300

    Add ListSecondaryAction

---
+++ src/components/wrapper-material-ui/ListItemSecondaryAction/ListItemSecondaryAction.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+
+// function ListItemSecondaryAction(props) {
+//     return <WListItemSecondaryAction {...props} />;
+// }
+// Exportando diretamente para manter a assinatura. Desta maneira ListItem aplica a classe necessária para nõa sobrepror o evento
+
+export default ListItemSecondaryAction;
