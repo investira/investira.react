088fe11b9a3759ae91ae01fc08cc47aebbda4ba9
Magno Valdetaro <mvaldetaro@gmail.com>
2021-10-25 10:55:17 -0300

    Update DeckView.jsx

--- src/components/DeckView/DeckView.jsx
+++ src/components/DeckView/DeckView.jsx
@@ -1,18 +1,15 @@
 import React, { memo, useContext } from 'react';
 import { DeckContext } from '../';
 
-const DeckView = memo(
-    Object.assign(
-        props => {
-            const deckContext = useContext(DeckContext);
+const DeckView = memo(props => {
+    const deckContext = useContext(DeckContext);
 
-            return React.cloneElement(props.children, {
-                id: props.id,
-                ...deckContext
-            });
-        },
-        { displayName: 'DeckView' }
-    )
-);
+    return React.cloneElement(props.children, {
+        id: props.id,
+        ...deckContext
+    });
+});
+
+DeckView.displayName = 'DeckView';
 
 export default DeckView;
