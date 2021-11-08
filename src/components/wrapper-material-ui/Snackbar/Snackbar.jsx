8a02f1c8d3fbb07b60e513b6e01f8a5b6bc9893f
willlucas1512 <willlucas1512@gmail.com>
2021-11-08 18:53:58 -0300

    remove código inutilizado

--- src/components/wrapper-material-ui/Snackbar/Snackbar.jsx
+++ src/components/wrapper-material-ui/Snackbar/Snackbar.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { default as MuiSnackbar } from '@material-ui/core/Snackbar';
-import { default as wSlide } from '@material-ui/core/Slide';
-import { duration } from '@material-ui/core/styles/transitions';
-import { renders } from '../../../lib';
 
 function Snackbar(props) {
-    console.log('Snackbar', props);
     const { children, autoHideDuration, onClose, ...xPassThruAttrs } = props;
 
-    let xAutoHideDuration =
-        autoHideDuration > 0
-            ? autoHideDuration
-            : autoHideDuration === null
-            ? null
-            : renders.getTimeFromTextLength(messageLength);
-
-    console.log(xAutoHideDuration);
     return (
-        <MuiSnackbar
-            {...xPassThruAttrs}
-            //autoHideDuration={xAutoHideDuration}
-            onClose={onClose}>
+        <MuiSnackbar {...xPassThruAttrs} onClose={onClose}>
             {children}
         </MuiSnackbar>
     );
