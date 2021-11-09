7d24d740edcbeeed82db51526002c3fca52d34fc
willlucas1512 <willlucas1512@gmail.com>
2021-11-09 13:06:20 -0300

    add Button story

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import { addParameters } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import {
+    withKnobs,
+    select,
+    radios,
+    boolean,
+    text
+} from '@storybook/addon-knobs';
 import { Button } from '../components';
 
 export default {
     title: 'Button',
+    decorator: [withKnobs],
     component: Button
 };
 
-addParameters({
-    docs: {
-        inlineStories: true
-    }
-});
+export const Default = () => {
+    const optionsColor = {
+        inherit: 'inherit',
+        primary: 'primary',
+        secondary: 'secondary',
+        success: 'success',
+        error: 'error',
+        info: 'info',
+        warning: 'warning'
+    };
 
-export const Default = () => <Button color={'primary'}> Button</Button>;
+    const optionsSize = {
+        small: 'small',
+        medium: 'medium',
+        large: 'large'
+    };
+
+    const optionsVariant = {
+        contained: 'contained',
+        outlined: 'outlined',
+        text: 'text'
+    };
+
+    return (
+        <Button
+            disabled={boolean('Disabled', false)}
+            disableElevation={boolean('disableElevation', false)}
+            disableFocusRipple={boolean('disableFocusRipple', false)}
+            disableRipple={boolean('disableRipple', false)}
+            fullWidth={boolean('fullWidth', false)}
+            variant={radios('Tipo', optionsVariant, 'text')}
+            size={radios('Tamanho', optionsSize, 'medium')}
+            color={select('Cor', optionsColor, 'primary')}>
+            {text('Label', 'Botão')}
+        </Button>
+    );
+};
