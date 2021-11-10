8c5e93c66dd6089caac2eb098ff99aa661ca17fd
willlucas1512 <willlucas1512@gmail.com>
2021-11-10 15:11:17 -0300

    Create InfoBool.stories.js

---
+++ src/stories/InfoBool.stories.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { InfoBool } from '../components';
+import {
+    withKnobs,
+    select,
+    radios,
+    boolean,
+    text
+} from '@storybook/addon-knobs';
+
+export default {
+    title: 'InfoBool',
+    parameters: {
+        componentSubtitle: 'Display de informações de apenas dois estados'
+    },
+    decorators: [withKnobs],
+    component: InfoBool
+};
+
+export const Default = args => {
+    const optionsColor = {
+        textPrimary: 'textPrimary',
+        textSecondary: 'textSecondary'
+    };
+    const optionsVariant = {
+        button: 'button',
+        caption: 'caption',
+        body1: 'body1',
+        body2: 'body2',
+        h6: 'h6',
+        h5: 'h5',
+        h4: 'h4',
+        h3: 'h3',
+        h2: 'h2',
+        h1: 'h1'
+    };
+    return (
+        <InfoBool
+            value={boolean('Valor', false)}
+            label={text('Label', 'Label')}
+            gutter={boolean('Margem', false)}
+            labelColor={radios('Cor da label', optionsColor, 'textSecondary')}
+            variant={select(
+                'Tamanho da fonte da label',
+                optionsVariant,
+                'caption'
+            )}
+            {...args}
+        />
+    );
+};
