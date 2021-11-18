8483a91a7e7756b6434f717fe054224dd28bf881
willlucas1512 <willlucas1512@gmail.com>
2021-11-18 18:46:00 -0300

    Create PasswordField.stories.js

---
+++ src/stories/PasswordField.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { PasswordField } from '../components';
+
+export default {
+    title: 'PasswordField',
+    component: PasswordField,
+    decorators: [withKnobs]
+};
+
+export const Default = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: '#191b2a',
+                padding: '16px',
+                width: '239px',
+                borderRadius: '10px'
+            }}>
+            <PasswordField />
+        </div>
+    );
+};
