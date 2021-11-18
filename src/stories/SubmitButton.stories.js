485cbb34a6bf7ac8a567f5c835c0fc9572f3cf9d
willlucas1512 <willlucas1512@gmail.com>
2021-11-18 18:45:58 -0300

    Create SubmitButton.stories.js

---
+++ src/stories/SubmitButton.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { boolean, radios, text, withKnobs } from '@storybook/addon-knobs';
+import { SubmitButton } from '../components';
+import { buttons } from './options';
+
+export default {
+    title: 'SubmitButton',
+    component: SubmitButton,
+    decorators: [withKnobs]
+};
+
+export const Default = () => {
+    return (
+        <SubmitButton
+            disabled={boolean('Desabilitado', false)}
+            isSubmitting={boolean('Está submetendo?', false)}
+            variant={radios('Tipo', buttons, 'outlined')}>
+            {text('Label', 'Submeter')}
+        </SubmitButton>
+    );
+};
