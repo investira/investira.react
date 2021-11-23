7700a58613fe16513e239dd0f953ce2147eb2d66
willlucas1512 <willlucas1512@gmail.com>
2021-11-23 17:56:17 -0300

    Create SearchBox.stories.js

---
+++ src/stories/SearchBox.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { boolean, text, withKnobs } from '@storybook/addon-knobs';
+import { SearchBox } from '../components';
+
+export default {
+    title: 'SearchBox',
+    component: SearchBox,
+    decorators: [withKnobs]
+};
+
+export const Default = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: '#191b2a',
+                padding: '16px',
+                borderRadius: '10px'
+            }}>
+            <SearchBox
+                filter={boolean('Filtro', false)}
+                placeholder={text('Placeholder', 'Pesquise aqui')}
+            />
+        </div>
+    );
+};
