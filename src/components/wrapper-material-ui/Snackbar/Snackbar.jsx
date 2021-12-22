4fd3cfcd549af54452d1f4dc29446c19bc136c60
Magno Valdetaro <mvaldetaro@gmail.com>
2021-12-22 11:39:27 -0300

    Resolve a mensagem não auto esconder

--- src/components/wrapper-material-ui/Snackbar/Snackbar.jsx
+++ src/components/wrapper-material-ui/Snackbar/Snackbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { default as MuiSnackbar } from '@material-ui/core/Snackbar';
 
 function Snackbar(props) {
-    const { children, autoHideDuration, onClose, ...xPassThruAttrs } = props;
+    const { children, onClose, ...xPassThruAttrs } = props;
 
     return (
         <MuiSnackbar {...xPassThruAttrs} onClose={onClose}>
